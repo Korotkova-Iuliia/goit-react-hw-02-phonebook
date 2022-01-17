9beed68a004b4939a283af85101cff97c6ff9520
Korotkova-Iuliia <3589900@gmail.com>
2022-01-17 19:16:29 +0200

    add contact

--- src/components/App.js
+++ src/components/App.js
@@ -9,10 +9,10 @@ import MyForm from "./MyForm/MyForm";
 class App extends Component {
   state = {
     contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     name: "",
     number: "",
@@ -38,10 +38,9 @@ class App extends Component {
   };
   render() {
     const { contacts } = this.state;
-    console.log(this.state.contacts);
+    console.log(this.state.contacts.id);
     console.log(contacts);
-    // const valuesdf = this.handleSubmit();
-    // console.log(valuesdf);
+
     return (
       <div>
         <GlobalStyle />
